d2824ef0d152cb804bec7695d2b69f94847a6a3f
zinianly-aide <zinianly@gmail.com>
2026-09-18 22:51:29 +0800

    feat(streaming): export latent chunk sink

--- wan/streaming/__init__.py
+++ wan/streaming/__init__.py
@@ -1,10 +1,11 @@
-from .events import GenerationEvent, GenerationEventSink, LatentChunkEvent
+from .events import GenerationEvent, GenerationEventSink, LatentChunkEvent, LatentChunkSink
 from .frame_bridge import FrameBridgeServer, FrameSnapshot, LatestFrameStore
 
 __all__ = [
     "GenerationEvent",
     "GenerationEventSink",
     "LatentChunkEvent",
+    "LatentChunkSink",
     "FrameBridgeServer",
     "FrameSnapshot",
     "LatestFrameStore",
